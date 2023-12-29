ecfe0d92f0c170b221b76e927dce6e2761732a90
22521518 <22521518@gm.uit.edu.vn>
2023-12-29 15:49:21 +0700

    adjsut font of player btn in leaderboard

--- GameplayForm/LeaderBoardForm.cs
+++ GameplayForm/LeaderBoardForm.cs
@@ -22,7 +22,7 @@ namespace WindowForm
             TitleLabel.Font = new Font (MainWindow.cFont.Alkhemikal, 15, FontStyle.Bold);
 
             ModeFilterLabel.Font = CloseButton.Font = AllModeButton.Font = ModeHumanFilterButton.Font = ModeClassicFilterButton.Font = new Font (MainWindow.cFont.Alkhemikal, 13, FontStyle.Regular);
-            NameFilterLabel.Font = NameFilterButton.Font = new Font (MainWindow.cFont.Alkhemikal, 10, FontStyle.Regular);
+            PlayerBoardButton.Font = NameFilterLabel.Font = NameFilterButton.Font = new Font (MainWindow.cFont.Alkhemikal, 10, FontStyle.Regular);
             LeaderBoard.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font(MainWindow.cFont.Alkhemikal, 20, System.Drawing.FontStyle.Bold);
             LeaderBoard.DefaultCellStyle.Font = new System.Drawing.Font(MainWindow.cFont.Alkhemikal, 13, System.Drawing.FontStyle.Bold);
 
